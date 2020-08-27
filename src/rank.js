470bdf0ae02eddf8f83b774d89be987aa7dcc052
Teddy <1294911641@qq.com>
2020-08-27 19:46:33 +0800

    [Homework][Teddy] refactor : extract filter method to countResultByNegativeProfit function

--- src/rank.js
+++ src/rank.js
@@ -23,12 +23,16 @@ function hasChina (history) {
   return history.some(v => 'china' === v.zone);
 }
 
+function countResultByNegativeProfit(history) {
+  return history.filter(v => v.profit < 0).length;
+}
+
 function captainHistoryRisk (voyage, history) {
   let result = 1;
   if (history.length < 5) {
     result += 4;
   }
-  result += history.filter(v => v.profit < 0).length;
+  result += countResultByNegativeProfit(history);
   if (voyage.zone === 'china' && hasChina(history)) {
     result -= 2;
   }

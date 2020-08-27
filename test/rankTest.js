a25699e961336c0fb1a8299d76482f0576388e9f
Teddy <1294911641@qq.com>
2020-08-27 17:52:22 +0800

    first commit

---
+++ test/rankTest.js
@@ -0,0 +1,39 @@
+import {rating} from "../src/rank";
+
+const rankTest = require('ava');
+
+// rankTest('foo', t => {
+//   t.pass();
+// });
+//
+// rankTest('bar', async t => {
+//   const bar = Promise.resolve('bar');
+//   t.is(await bar, 'bar');
+// });
+const history = [
+  {
+    zone: 'east-indies',
+    profit: 5,
+  },{
+    zone: 'west-indies',
+    profit: 15,
+  },{
+    zone: 'china',
+    profit: -2,
+  },
+  {
+    zone: 'west-africa',
+    profit: 7,
+  },
+];
+
+rankTest('voyage:zone:west-indies,length:10 \n',t =>{
+  const voyage = {
+    zone: 'west-indies',
+    length: 10,
+  };
+
+  const result = rating(voyage,history);
+
+  t.is(result,'B')
+});

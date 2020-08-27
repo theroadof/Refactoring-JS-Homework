1a865a0dd86dc83e25e42606ef6dead299e539f9
Teddy <1294911641@qq.com>
2020-08-27 20:03:47 +0800

    [Homework][Teddy] test : add should return Employee cannot be of type test when toString given name teddy, type test test

--- test/employeeTest.js
+++ test/employeeTest.js
@@ -10,3 +10,15 @@ test('should return teddy (engineer) when toString given name teddy, type engine
 
     t.is(employee.toString(),'teddy (engineer)')
 });
+
+test('should return Employee cannot be of type test when toString given name teddy, type test',t=>{
+    let name = 'teddy';
+    let type = 'test';
+
+    try {
+        new Employee(name,type);
+        t.fail();
+    }catch (e) {
+        t.is(e.message,'Employee cannot be of type test')
+    }
+});

dc71789527ab0eaa454d3d8e5af33f4566090f24
Teddy <1294911641@qq.com>
2020-08-28 11:34:20 +0800

    [Homework][Teddy] test : add should return 3 when deliveryDate given anOrder.deliveryState: NH, isRush: true test

--- test/deliveryTest.js
+++ test/deliveryTest.js
@@ -17,3 +17,19 @@ test('should return 2 when deliveryDate given anOrder.deliveryState: MA, isRush:
 
     t.is(result,2);
 });
+
+test('should return 3 when deliveryDate given anOrder.deliveryState: NH, isRush: true',t=>{
+    const anOrder = {
+        deliveryState: 'NH',
+        placedOn: {
+            plusDays: (day)=>{
+                return day;
+            }
+        }
+    };
+    const isRush = true;
+
+    const result = deliveryDate(anOrder,isRush);
+
+    t.is(result,3);
+});
